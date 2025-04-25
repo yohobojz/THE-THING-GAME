196b2f2061027fa51026893314a3ca6060a35027
yohobojz <amandella@tcbns.com>
2025-04-25 08:58:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = new Server(server);
 app.use(express.static('public'));
 
 const lobbies = {}; // { lobbyId: { players: [], host: socket.id } }
-const playerData = {}; // { socketId: { lobbyId, hasCalledMeeting, messagesThisRound, currentRoom } }
+const playerData = {}; // { socketId: { lobbyId, hasCalledMeeting, messagesThisRound, currentRoom, role } }
 const emergencyMeeting = {}; // { lobbyId: socketId or null }
 
 io.on('connection', (socket) => {
@@ -21,14 +21,13 @@ io.on('connection', (socket) => {
       players: [socket.id],
       host: socket.id
     };
-
     playerData[socket.id] = {
       lobbyId,
       hasCalledMeeting: false,
       messagesThisRound: 0,
-      currentRoom: socket.id
+      currentRoom: socket.id,
+      role: 'unknown'
     };
-
     emergencyMeeting[lobbyId] = null;
 
     socket.join(lobbyId);
@@ -43,7 +42,8 @@ io.on('connection', (socket) => {
         lobbyId,
         hasCalledMeeting: false,
         messagesThisRound: 0,
-        currentRoom: socket.id
+        currentRoom: socket.id,
+        role: 'unknown'
       };
       socket.join(lobbyId);
       socket.emit('lobbyJoined', { lobbyId, isHost: false });
@@ -72,4 +72,66 @@ io.on('connection', (socket) => {
     data.messagesThisRound++;
 
     if (emergencyMeeting[lobbyId]) {
-      io.to(lobbyId).emit('
+      io.to(lobbyId).emit('receiveMessage', {
+        from: socket.id,
+        text: msg
+      });
+    } else {
+      const recipients = Object.keys(playerData).filter(
+        id => playerData[id].lobbyId === lobbyId && playerData[id].currentRoom === currentRoom
+      );
+      recipients.forEach(id => {
+        io.to(id).emit('receiveMessage', {
+          from: socket.id,
+          text: msg
+        });
+      });
+    }
+  });
+
+  socket.on('callEmergencyMeeting', () => {
+    const data = playerData[socket.id];
+    if (!data || data.hasCalledMeeting || emergencyMeeting[data.lobbyId]) return;
+
+    data.hasCalledMeeting = true;
+    emergencyMeeting[data.lobbyId] = socket.id;
+
+    io.to(data.lobbyId).emit('emergencyMeetingStarted', {
+      calledBy: socket.id
+    });
+  });
+
+  socket.on('disconnect', () => {
+    for (const lobbyId in lobbies) {
+      const lobby = lobbies[lobbyId];
+      lobby.players = lobby.players.filter(id => id !== socket.id);
+      delete playerData[socket.id];
+
+      io.to(lobbyId).emit('playerListUpdated', lobby.players);
+
+      if (lobby.players.length === 0) {
+        delete lobbies[lobbyId];
+        delete emergencyMeeting[lobbyId];
+      }
+    }
+    console.log('A user disconnected:', socket.id);
+  });
+
+  socket.on('startGame', (lobbyId) => {
+    const lobby = lobbies[lobbyId];
+    if (!lobby) return;
+
+    const players = lobby.players;
+
+    players.forEach((playerId, index) => {
+      io.to(playerId).emit('gameStarted', {
+        playerNumber: index + 1,
+        totalPlayers: players.length
+      });
+    });
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
